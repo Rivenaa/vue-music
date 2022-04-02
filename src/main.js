bbd127fd170f2828ccc507939d2cd590603f2e79
Rivenaa <347153785@qq.com>
2022-04-02 14:20:44 +0800

    feat: init

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/**
+ * 全局导入scss
+ */
+import '@/assets/scss/index.scss'
+
 createApp(App).use(store).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  /**
+   * 全局引入scss变量和mixin
+   */
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData:
+          '@import "@/assets/scss/variable.scss";@imprt "@/assets/scss/mixin.scss";'
+      }
+    }
+  }
 })
